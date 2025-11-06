83c55781830ede4500b15d8d4a9c5eb83e5d584a
Juan Garcia Rodenas <jgarrod3@epsa.upv.es>
2025-11-06 09:05:22 +0100

    Añadida interfaz genérica List2.0

--- List.h
+++ List.h
@@ -1,12 +1,21 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdexcepts>
+
 template <typename T> 
 class List {
     public:
 	   virtual ~List() {}
 
 	   virtual void insert(int pos, T e) = 0;
+	   virtual append(T e) = 0;
+	   virtual void T remove(int pos) = 0;
+	   virtual const T get(int pos) = 0;
+	   virtual const int search(T e) = 0;
+	   virtual const bool empty() = 0;
+	   virtual const int size() = 0;
+
 };
 
 #endif
