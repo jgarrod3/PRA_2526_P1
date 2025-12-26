c16fd7dfd755ec185ce09f96ff9d7546583121c0
Juan Garcia Rodenas <jgarrod3@epsa.upv.es>
2025-12-26 10:52:21 +0100

    Añadido Makefile y código de test de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -44,7 +44,7 @@ class ListArray : public List<T> {
 			}
 
 			for (int i = n; i > pos; i--) {
-				arr[i] = arr[i--];
+				arr[i] = arr[i - 1];
 			}
 			arr[pos] = e;
 			n++;
@@ -66,14 +66,14 @@ class ListArray : public List<T> {
 			T elemento_eliminado = arr[pos];
 
 			for (int i = pos; i < n-1; i++) {
-				arr[i] = arr[i++];
+				arr[i] = arr[i + 1];
 			}
 			n--;
 
 			return elemento_eliminado;
 		}
 
-		T get(int pos) override {
+		T get(int pos) const override {
 			if (pos < 0 || pos >= n) {
 				throw std::out_of_range("La posición no es válida");
 			}
@@ -81,7 +81,7 @@ class ListArray : public List<T> {
 			return arr[pos];
 		}
 
-		int search(T e) override {
+		int search(T e) const override {
 			for (int i = 0; i < n; i++) {
 				if (arr[i] == e) {
 					return i;
@@ -97,21 +97,24 @@ class ListArray : public List<T> {
 			
 		}
 	
-		bool empty() override {
+		bool empty() const override {
 			return n == 0;
 		}
 
-		int size() override {
+		int size() const override {
 			return n;
 		}
 
 		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list) {
-			out << "[";
+			if (list.n == 0) {
+				out << "List => []";
+				return out;
+			}
+
+			out << "List => [\n";
 			for (int i = 0; i < list.n; i++) {
-				out << list.arr[i];
-				if (i < list.n-1) {
-					out << ", ";
-				}
+				out << " " << list.arr[i] << "\n";
+				
 			}
 			out << "]";
 			return out;
