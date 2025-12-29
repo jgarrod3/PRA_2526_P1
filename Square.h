ac74dd961fcd3f39a58f8a2a38a312b7287d1366
Juan Garcia Rodenas <jgarrod3@epsa.upv.es>
2025-12-29 15:51:31 +0100

    Añdido square.h actualizado

---
+++ Square.h
@@ -0,0 +1,23 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <iostream>
+#include "Point2D.h"
+#include "Rectangle.h"
+
+class Square : public Rectangle {
+	private:
+		static bool check(Point2D* vertices);
+
+	public:
+		Square();
+		Square(std::string color, Point2D* vertices);
+
+		void set_vertices(Point2D* vertices) override;
+		void print(std::ostream &out) const override;
+
+		friend std::ostream& operator<<(std::ostream &out, const Square &square);
+
+};
+
+#endif
